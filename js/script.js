1f6fcaf73b01dd1f06d284c0545f6d91a4223cba
salah eddib <salaheddib.55@gmail.com>
2020-02-29 01:32:41 +0100

    start on responsive
    daily commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 var menuItem = document.getElementById("textarea__lists").getElementsByTagName("il");
 var bigText = document.getElementById("textarea__bigh");
+var smallText = document.getElementById("textarea__smallh");
+var button = document.getElementById("textarea__btn")
 
 
 menuItem[1].addEventListener("click", selectItem);
@@ -10,6 +12,10 @@ function selectItem() {
     this.classList.add("list1");
     menuItem[0].classList.remove("list1");
     menuItem[2].classList.remove("list1");
+    bigText.innerHTML = "We add value to your businesses "
+    smallText.innerHTML = "We have a passion for producing distinctive"
+    button.innerHTML = "start a projet"
+
 
 
 
@@ -19,11 +25,19 @@ function selectItem1() {
     this.classList.add("list1");
     menuItem[1].classList.remove("list1");
     menuItem[2].classList.remove("list1");
+    bigText.innerHTML = "POWERFUL INSIGHTS INTO YOUR TEAM"
+    smallText.innerHTML = "Project planning and time tracking for agile teams"
+    button.innerHTML = "SCHEDULE A DEMO"
+
+
 }
 
 function selectItem2() {
     this.classList.add("list1");
     menuItem[0].classList.remove("list1");
     menuItem[1].classList.remove("list1");
+    bigText.innerHTML = "DIGITAL BEAUTY INSIDE AND OUT"
+    smallText.innerHTML = "We build websites that build brands"
+    button.innerHTML = "start your brand"
 
 }
