291ea53d76f526eb2f45af3fadcce8f55eaa9195
salah eddib <salaheddib.55@gmail.com>
2020-03-01 05:22:45 +0100

    end of responsive (by using javascript)

--- js/script.js
+++ js/script.js
@@ -1,20 +1,25 @@
-var menuItem = document.getElementById("textarea__lists").getElementsByTagName("il");
-var bigText = document.getElementById("textarea__bigh");
-var smallText = document.getElementById("textarea__smallh");
-var button = document.getElementById("textarea__btn")
+var menuItem = document.querySelector(".textarea__lists").getElementsByTagName("il");
+var bigText = document.querySelector(".textarea__bigh");
+var smallText = document.querySelector(".textarea__smallh");
+var button = document.querySelector(".textarea__btn");
+var burger = document.querySelector(".navbar__burger");
+var menu = document.querySelector(".lists");
+var closeBurger = document.querySelector(".navbar__close");
 
 
 menuItem[1].addEventListener("click", selectItem);
 menuItem[0].addEventListener("click", selectItem1);
 menuItem[2].addEventListener("click", selectItem2);
+burger.addEventListener("click", humburger)
+closeBurger.addEventListener("click", closeHumburger)
 
 function selectItem() {
     this.classList.add("list1");
     menuItem[0].classList.remove("list1");
     menuItem[2].classList.remove("list1");
-    bigText.innerHTML = "We add value to your businesses "
-    smallText.innerHTML = "We have a passion for producing distinctive"
-    button.innerHTML = "start a projet"
+    bigText.innerHTML = "We add value to your businesses ";
+    smallText.innerHTML = "We have a passion for producing distinctive";
+    button.innerHTML = "start a projet";
 
 
 
@@ -25,9 +30,9 @@ function selectItem1() {
     this.classList.add("list1");
     menuItem[1].classList.remove("list1");
     menuItem[2].classList.remove("list1");
-    bigText.innerHTML = "POWERFUL INSIGHTS INTO YOUR TEAM"
-    smallText.innerHTML = "Project planning and time tracking for agile teams"
-    button.innerHTML = "SCHEDULE A DEMO"
+    bigText.innerHTML = "POWERFUL INSIGHTS INTO YOUR TEAM";
+    smallText.innerHTML = "Project planning and time tracking for agile teams";
+    button.innerHTML = "SCHEDULE A DEMO";
 
 
 }
@@ -36,8 +41,26 @@ function selectItem2() {
     this.classList.add("list1");
     menuItem[0].classList.remove("list1");
     menuItem[1].classList.remove("list1");
-    bigText.innerHTML = "DIGITAL BEAUTY INSIDE AND OUT"
-    smallText.innerHTML = "We build websites that build brands"
-    button.innerHTML = "start your brand"
+    bigText.innerHTML = "DIGITAL BEAUTY INSIDE AND OUT";
+    smallText.innerHTML = "We build websites that build brands";
+    button.innerHTML = "start your brand";
+
+}
+
+function humburger() {
+    this.classList.remove("show")
+    menu.classList.add("show");
+    this.classList.add("hide");
+    closeBurger.classList.add("show");
+
+
+
+}
+
+function closeHumburger() {
+    menu.classList.remove("show")
+    this.classList.remove("show")
+    burger.classList.add("show")
+
 
 }
